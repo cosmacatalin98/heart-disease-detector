d5353f41fc5d07e50ab52602744da6a89d047c2c
Cosma Catalin <cosmacatalin98@yahoo.com>
2020-09-21 12:31:20 +0300

    Create medical-record interface

---
+++ src/app/medical-record.ts
@@ -0,0 +1,15 @@
+export interface MedicalRecord {
+    age: number,
+    sex: number,
+    cpt: number,
+    rbp: number,
+    sc: number,
+    fbs: number,
+    rer: number,
+    mhra: number,
+    eia: number,
+    oldpeak: number,
+    tsotpe: number,
+    nomv: number,
+    thatl: number
+}
